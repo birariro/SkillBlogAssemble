aedcbd4ba70201d4e38a92bf06609e56e950c1d9
k4keye <vps32@naver.com>
2021-12-08 00:21:04 +0900

    viewpager2 sample add

---
+++ app/src/main/java/com/example/skillblogassemble/domain/model/BlogItem.kt
@@ -0,0 +1,8 @@
+package com.example.skillblogassemble.domain.model
+
+data class BlogItem(
+    val company:String,
+    val title : String,
+    val content : String,
+    val Color : Int
+)
--- app/src/main/java/com/example/skillblogassemble/presenter/MainActivity.kt
+++ app/src/main/java/com/example/skillblogassemble/presenter/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
 import com.example.skillblogassemble.R
+import com.example.skillblogassemble.domain.model.BlogItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,12 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val items = ArrayList<Int>().apply {
-            add(Color.parseColor("#ffff00"))
-            add(Color.parseColor("#5fdbdb"))
-            add(Color.parseColor("#45afdb"))
-            add(Color.parseColor("#5fdbdb"))
-            add(Color.parseColor("#45afdb"))
+        val items = ArrayList<BlogItem>().apply {
+            add(BlogItem("naver","플러터 사용기","할만했다" ,  Color.parseColor("#1DDB16")))
+            add(BlogItem("쿠팡","쿠팡 뭔가를 함","뭔가를 했다" ,  Color.parseColor("#FF0000")))
+            add(BlogItem("페이스북","페이스북 이름 바꿈","블록체인" ,  Color.parseColor("#0054FF")))
+            add(BlogItem("당근마켓","당근 근처 있음","이름 이유" ,  Color.parseColor("#FF5E00")))
         }
         var adapter = ViewPagerAdapter(items)
 
