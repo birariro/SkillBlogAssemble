7d2bf748c00a63fae80ca574a4c8cf3a9efcdd50
k4keye <vps32@naver.com>
2021-12-08 00:58:30 +0900

    viewmodel init

--- app/src/main/java/com/example/skillblogassemble/presenter/MainActivity.kt
+++ app/src/main/java/com/example/skillblogassemble/presenter/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.skillblogassemble.presenter
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.example.skillblogassemble.R
 import com.example.skillblogassemble.domain.model.BlogItem
@@ -10,15 +12,16 @@ import com.example.skillblogassemble.domain.model.BlogItem
 class MainActivity : AppCompatActivity() {
 
     val viewPager : ViewPager2  by lazy { findViewById(R.id.viewPager) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val items = ArrayList<BlogItem>().apply {
-            add(BlogItem("naver","플러터 사용기","할만했다" ,  Color.parseColor("#1DDB16")))
-            add(BlogItem("쿠팡","쿠팡 뭔가를 함","뭔가를 했다" ,  Color.parseColor("#FF0000")))
-            add(BlogItem("페이스북","페이스북 이름 바꿈","블록체인" ,  Color.parseColor("#0054FF")))
-            add(BlogItem("당근마켓","당근 근처 있음","이름 이유" ,  Color.parseColor("#FF5E00")))
-        }
+
+        val viewModel : MainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+
+        val items = viewModel.getPostItemList()
         var adapter = ViewPagerAdapter(items)
 
         viewPager.adapter = adapter
